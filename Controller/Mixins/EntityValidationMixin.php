82ea1c944e69fde73ee00c7d4b7697b12127b396
Vic Cherubini <vmc@leftnode.com>
2012-12-07 05:38:01 -0600

    New mixin that provides a nice function for checking validation of entities.

---
+++ Controller/Mixins/EntityValidationMixin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brightmarch\Bundle\RestfulBundle\Controller\Mixins;
+
+use Brightmarch\Bundle\RestfulBundle\Entity\Entity;
+use Brightmarch\Bundle\RestfulBundle\Exceptions\HttpBadSyntaxException;
+
+use Symfony\Component\Validator\ConstraintViolationList;
+
+trait EntityValidationMixin
+{
+
+    /**
+     * Tests the entity to see if there are any violations. If so, a HttpBadSyntaxException is thrown.
+     *
+     * @param Brightmarch\Bundle\RestfulBundle\Entity\Entity $entity
+     * @param string $message
+     * @return boolean
+     */
+    public function checkForViolations(Entity $entity, $message="")
+    {
+        $violations = $this->get('validator')
+            ->validate($entity);
+
+        if ($violations) {
+            $this->throwHttpBadSyntaxException($violation, $message);
+        }
+
+        return true;
+    }
+
+
+
+    private function throwHttpBadSyntaxException(ConstraintViolationList $violations, $message)
+    {
+        $exception = (new HttpBadSyntaxException($message))
+            ->setViolations($violations);
+
+        throw $exception;
+    }
+
+}
