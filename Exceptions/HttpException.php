7de9ea898eb12a4d7d8f1d10f2ed070ca0cbb7a9
Vic Cherubni <vmc@leftnode.com>
2012-12-07 04:59:08 -0600

    Can now get/set different violations.

--- Exceptions/HttpException.php
+++ Exceptions/HttpException.php
@@ -2,11 +2,19 @@
 
 namespace Brightmarch\Bundle\RestfulBundle\Exceptions;
 
+use Symfony\Component\Validator\ConstraintViolationList;
+
 class HttpException extends \Exception
 {
 
+    /** @var string */
     public $exceptionMessage = "";
+
+    /** @var integer */
     public $httpCode = 0;
+
+    /** @var array */
+    public $violations = [];
 
     public function __construct($message="", $code=0)
     {
@@ -14,6 +22,23 @@ class HttpException extends \Exception
 
         $this->exceptionMessage = $message;
         $this->httpCode = (int)$code;
+    }
+    
+    public function setViolations(ConstraintViolationList $violations)
+    {
+        $this->violations = [];
+
+        foreach ($violations as $violation) {
+            $propertyPath = $violation->getPropertyPath();
+            $this->violations[$propertyPath] = $violation->getMessage();
+        }
+
+        return $this;
+    }
+
+    public function getViolations()
+    {
+        return $this->violations;
     }
 
 }
